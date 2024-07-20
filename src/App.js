fca4915eb8b0f92fe852690a142c1db0e211a7f6
Joshua <joshuaking382001@gmail.com>
2024-07-20 16:03:27 +0530

    downtime_ui

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import Sign from './comp/Sign';
 import Reg from './comp/Reg';
 import Dashboard from './comp/Dashboard';
 import Add from './comp/Add';
-import {BrowserRouter as Router,Routes,Route,} from "react-router-dom";
+import Foot from './comp/Foot';
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import Logs from './comp/Logs';
 function App() {
   return (
     <div className="App">
-
-      <Routes> 
-            <Route path="/" element={<Dashboard/>}/>
-            <Route path="/add" element={<Add/>}/>
-            <Route path="/log" element={<Logs/>}/>
-            <Route path="/sign" element={<Sign />} />
-            <Route path="/reg" element={<Reg />} />
-            </Routes>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/log" element={<Logs />} />
+        <Route path="/sign" element={<Sign />} />
+        <Route path="/reg" element={<Reg />} />
+      </Routes>
+      <Foot />
     </div>
   );
 }
 
 export default App;
 
-    
--- src/comp/Add.jsx
+++ src/comp/Add.jsx
@@ -90,33 +90,6 @@ function Add() {
 
       </div>
 
-
-      <footer class="shadow backdrop-blur-md py-3 dark:bg-transparent ">
-        <div class="md:w-full max-w-screen-xl mx-auto md:py-1">
-          <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="https://digiplusagency.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-              <img src={foot} class="md:h-24" alt="Logo" />
-            </a>
-            <ul class="xl:flex xl:flex-wrap xl:pr-4 md:items-center  text-sm font-medium text-gray-500 dark:text-black">
-              <li>
-                <a href="https://digiplusagency.com/digiplus-team/" class="hover:underline me-4 md:me-6">About</a>
-              </li>
-              <li>
-                <a href="https://digiplusagency.com/privacy-policy/" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-              </li>
-              <li>
-                <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-              </li>
-              <li>
-                <a href="#" class="hover:underline">Contact</a>
-              </li>
-            </ul>
-          </div>
-          <hr class="h-2 border-gray-200 sm:mx-auto dark:border-gray-700" />
-          <span class="block h-2 text-sm text-gray-500 sm:text-center dark:text-black">© 2024 <a href="https://digiplusagency.com/" class="hover:underline">digiplusagency</a>. All Rights Reserved.</span>
-        </div>
-      </footer>
-
     </div>
   )
 }
