9ae00c96e184ee3783fb1a476e87da43672d5756
Joshua <joshuaking382001@gmail.com>
2024-07-22 13:37:02 +0530

    downtime_ui

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import Reg from './comp/Reg';
 import Dashboard from './comp/Dashboard';
 import Add from './comp/Add';
 import Foot from './comp/Foot';
+import Account from './comp/Account';
 import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import Logs from './comp/Logs';
 function App() {
   return (
     <div className="App">
-      <Routes>
+      <Account/>
+      {/*<Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/add" element={<Add />} />
         <Route path="/log" element={<Logs />} />
         <Route path="/sign" element={<Sign />} />
         <Route path="/reg" element={<Reg />} />
       </Routes>
-      <Foot />
+      <Foot />*/}
     </div>
   );
 }
--- src/comp/Dashboard.jsx
+++ src/comp/Dashboard.jsx
@@ -116,7 +116,7 @@ function Dashboard() {
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr class="odd:bg-white odd:dark:bg-white even:bg-gray-50 even:dark:bg-gray-800 border-b">
+                                        <tr class="odd:bg-white odd:dark:bg-white even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
                                                 http//test.com
                                             </th>
